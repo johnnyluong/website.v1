f5859e083de1313ac99a461deff5586e80eaeac0
Johnny Luong <jluong.dev@gmail.com>
2023-10-01 05:24:10 -0700

    fixed column width

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='scroll-smooth'>
-      <body className={`${albert.variable} font-albert`}>{children}</body>
+      <body className={`${albert.variable} font-albert leading-relaxed antialiased selection:bg-sky-300 selection:text-sky-900`}>{children}</body>
     </html>
   )
 }
